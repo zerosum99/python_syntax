2b333354639965b87033dfb02aa523d76cbe1abf
zerosum99 <myjlms99@gmail.com>
2017-08-03 11:36:06 +0900

    666666666

--- doc_test.py
+++ doc_test.py
@@ -9,6 +9,9 @@
 >>> print(list(range(20))) # doctest: +ELLIPSIS
 ...                 # doctest: +NORMALIZE_WHITESPACE
 [0,    1, ...,   18,    19]
+>>> print(list(range(5)) + list(range(10,20)) + list(range(30,40)) + list(range(50,60)))
+... # doctest: +ELLIPSIS
+[0, ..., 4, 10, ..., 19, 30, ..., 39, 50, ..., 59]
 '''
 
 
